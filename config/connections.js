204551d2e78f161e8f3482092ac2d00cc47e955c
AmalThomasThettayil <amal.thms@gmail.com>
2022-07-01 13:46:49 +0530

    mongodb atlas connection

--- config/connections.js
+++ config/connections.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/toycart",{
+mongoose.connect("mongodb+srv://toycart:amal1234@cluster0.l2ceq.mongodb.net/toycart",{
     useNewUrlParser:true
 }).then(()=>{
     console.log('connection Successfull')
 }).catch((e)=>{
     console.log('No Connection'+e);
 })
+// mongodb://127.0.0.1:27017/toycart
