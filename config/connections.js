5a37faa36f459910c302fb3eda95ea6aee4d5641
AmalThomasThettayil <amal.thms@gmail.com>
2022-07-02 10:52:37 +0530

    connection error submit

--- config/connections.js
+++ config/connections.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://toycart:amal1234@cluster0.l2ceq.mongodb.net/?retryWrites=true&w=majority",{
     useNewUrlParser:true
 }).then(()=>{
-    console.log('connection Successfull')
+    console.log('connection is Successfull')
 }).catch((e)=>{
     console.log('No Connection'+e);
 })
